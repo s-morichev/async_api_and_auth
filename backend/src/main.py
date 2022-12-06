2063e71977d822f56de7fbc7c4558e89e2177206
RomanBorovskiy <59736627+RomanBorovskiy@users.noreply.github.com>
2022-12-06 13:30:11 +0300

    ver 1.1 (#45)
    * Rename README.md to _README.md
    
    * Create README.md
    
    * Create .gitkeep
    
    all files from template
    
    * Initial commit
    
    * Create initial structure
    
    * Add info in readme
    
    * Add requirements.txt
    
    * Add Makefile
    
    * Add data models
    
    * Apply suggestions from code review
    
    * Add ES Schema
    
    * set docker compose
    
    * delete uncompressed dump
    
    * Change ES Schema v.2
    
    * fix redis config
    
    * add some docstrings
    
    * update .env.example
    
    * Change models v2
    
    * Change models v2
    
    * Change models v2
    
    * use BaseOrjsonModel as basic for all models
    
    * copy etl old project
    
    * add to etl 2 new pipelines
    
    * fix errors, upgrade elastic library
    
    * fix settings module, formatting
    
    * ready for docker
    
    * reformating for best view
    
    * change es_create_index_if_not_exist() function for catch exceptions
    
    * add backoff on all_ready_for_etl() function
    
    * Change ES persons schema. Add "settings", add field "movies"
    
    * update persons ES schema file
    
    * change SQL EX_PERSONS_SQL. Formatting sql strings for pretty view
    
    * Change FPerson class, add RoleMovies and Movie class
    
    * formatting :(
    
    * formatting
    
    * Change readme, Makefile. Add black and flake configs (#31)
    
    * Backend add (#32)
    
    * initial commit fore mvp branch
    
    * change for load new aioredis
    
    * add result_name in BaseService.init_result_model(cls)
    
    * fix error with page_restriction. Add default and max page size
    
    * change ResultModel: name to Result: for fun)
    
    * merge genres and persons endpoints
    
    * split api schemas and dto
    
    * delete api_models
    
    * add settins: BaseSettings pydantic
    
    * add settings for backend
    
    * add logger, add use_cache in base_service
    
    * formatting
    
    * add env.local.example, add python-dotenv to requirements.txt
    
    * delete web, db :(((
    
    * change aioredis version
    
    * Films service (#28)
    
    * film service v1
    
    * add docstrings, fixes
    
    * rewrite, split service
    
    * fix inconsistencies, formatting
    
    * use kwargs instead of query_dict
    
    * use pydantic generic models
    
    * fixes, cleanup
    
    * Backend fix (#29)
    
    * merge genres and persons services
    
    * fix: RESULT_MODEL cant be None!
    
    * fix: remove BASE_MODEL field. Its now property calculated from RESULT_MODEL
    
    * fix: validate_pagination return None if success
    
    * add constants in params Query
    
    * add docstrings
    
    * refactor: BaseDTO to CoreModel. rename base_dto.py to core_model.py, move to core
    
    * add params module with PageParams and QueryPageParams
    
    * add params module with PageParams and QueryPageParams
    
    * add Postman tests
    
    * add openapi description
    
    * fix by review
    
    * remove double logging config (#30)
    
    * remove double logging config, remove service_logger. Now all logging config in logger
    
    * change debug message order
    
    Co-authored-by: s-morichev <115094382+s-morichev@users.noreply.github.com>
    
    * tags, flake8 config, fix logic
    
    * fix depends in base_service.get_service (#33)
    
    * Develop text (#39)
    
    * add readme text
    
    * add summary in routers
    
    * formating
    
    * add fuzziness to persons query
    
    * Develop service structure (#42)
    
    * Предложение по структуре сервисов
    
    * docker for production
    
    * Предложение по структуре сервисов
    
    * make backend and etl like single repos
    
    Co-authored-by: Sergey Morichev <s.morichev@yandex.ru>
    
    * Update README.md
    
    Co-authored-by: s-morichev <115094382+s-morichev@users.noreply.github.com>
    Co-authored-by: Sergey Morichev <s.morichev@yandex.ru>

---
+++ backend/src/main.py
@@ -0,0 +1,58 @@
+import logging
+
+import aioredis
+import uvicorn as uvicorn
+from elasticsearch import AsyncElasticsearch
+from fastapi import FastAPI
+from fastapi.responses import ORJSONResponse
+
+from api.v1 import films, genres, persons
+from core.config import settings
+from core.logger import LOGGING
+from db import elastic, redis
+
+tags_metadata = [
+    {"name": "Фильмы", "description": "Запросы по фильмам"},
+    {"name": "Персоны", "description": "Запросы по персонам"},
+    {"name": "Жанры", "description": "Запросы по жанрам"},
+]
+
+logger = logging.getLogger(__name__)
+
+app = FastAPI(
+    title=settings.PROJECT_NAME,
+    version="1.0.0",
+    openapi_tags=tags_metadata,
+    docs_url="/api/openapi",
+    openapi_url="/api/openapi.json",
+    default_response_class=ORJSONResponse,
+)
+
+
+@app.on_event("startup")
+async def startup():
+    logger.info("service start")
+    redis.redis = await aioredis.from_url(settings.REDIS_URI)
+    elastic.es = AsyncElasticsearch(hosts=[settings.ES_URI])
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    # Отключаемся от баз при выключении сервера
+    await redis.redis.close()
+    await elastic.es.close()
+    logger.info("service shutdown")
+
+
+app.include_router(films.router, prefix="/api/v1/films", tags=["Фильмы"])
+app.include_router(persons.router, prefix="/api/v1/persons", tags=["Персоны"])
+app.include_router(genres.router, prefix="/api/v1/genres", tags=["Жанры"])
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "main:app",
+        host="0.0.0.0",
+        port=8000,
+        log_config=LOGGING,
+        log_level=logging.DEBUG,
+    )
