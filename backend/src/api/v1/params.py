5ed47d52cc5d4071f533567186690564fd6bc8a0
RomanBorovskiy <59736627+RomanBorovskiy@users.noreply.github.com>
2022-12-13 16:20:05 +0300

    fix lte error im max_page_size

--- backend/src/api/v1/params.py
+++ backend/src/api/v1/params.py
@@ -11,7 +11,7 @@ from core.utils import validate_pagination
 class PageParams:
     page_number: int = Query(default=1, alias=KEY_PAGE_NUM, title="number of page (pagination)", ge=1)
     page_size: int = Query(
-        default=DEFAULT_PAGE_SIZE, alias=KEY_PAGE_SIZE, title="count of results rows", ge=1, lte=MAX_PAGE_SIZE
+        default=DEFAULT_PAGE_SIZE, alias=KEY_PAGE_SIZE, title="count of results rows", ge=1, le=MAX_PAGE_SIZE
     )
 
     def check_pagination(self):
