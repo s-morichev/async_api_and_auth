114787f6a882dcb6cc4ccc06e1874c0cc4712d14
RomanBorovskiy <59736627+RomanBorovskiy@users.noreply.github.com>
2022-12-15 14:33:32 +0300

    fix bug with persons api

--- backend/src/api/v1/persons.py
+++ backend/src/api/v1/persons.py
@@ -10,7 +10,32 @@ from services.persons import FilmsByPersonService, PersonByIdService, PersonSear
 router = APIRouter()
 
 
-@router.get("/search/{person_id}", response_model=ExtendedPerson, summary="get one person by id=:person_id")
+@router.get(
+    "/search", response_model=ManyResponse[ExtendedPerson], summary="get many persons with full name like :query_string"
+)
+async def person_search(
+    params: QueryPageParams = Depends(),
+    service: PersonSearchService = Depends(PersonSearchService.get_service),
+) -> ManyResponse[ExtendedPerson]:
+    """
+    Поиск персон по имени
+    - query - поисковая строка
+    - **page[number]**: номер страницы
+    - **page[size]**: количество записей на странице
+    """
+
+    params.check_pagination()
+
+    answer = await service.get(page_num=params.page_number, page_size=params.page_size, query=params.query)
+
+    if not answer:
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail=f"persons for '{params.query}' not found")
+
+    lst_person = [ExtendedPerson(**dto.dict()) for dto in answer.result]
+    return ManyResponse[ExtendedPerson](total=answer.total, result=lst_person)
+
+
+@router.get("/{person_id}", response_model=ExtendedPerson, summary="get one person by id=:person_id")
 async def person_by_id(
     person_id: UUID, service: PersonByIdService = Depends(PersonByIdService.get_service)
 ) -> ExtendedPerson:
@@ -48,28 +73,3 @@ async def films_by_person(
     lst_film = [ImdbFilm(**dto.dict()) for dto in answer.result]
 
     return ManyResponse[ImdbFilm](total=answer.total, result=lst_film)
-
-
-@router.get(
-    "/", response_model=ManyResponse[ExtendedPerson], summary="get many persons with full name like :query_string"
-)
-async def person_search(
-    params: QueryPageParams = Depends(),
-    service: PersonSearchService = Depends(PersonSearchService.get_service),
-) -> ManyResponse[ExtendedPerson]:
-    """
-    Поиск персон по имени
-    - query - поисковая строка
-    - **page[number]**: номер страницы
-    - **page[size]**: количество записей на странице
-    """
-
-    params.check_pagination()
-
-    answer = await service.get(page_num=params.page_number, page_size=params.page_size, query=params.query)
-
-    if not answer:
-        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail=f"persons for '{params.query}' not found")
-
-    lst_person = [ExtendedPerson(**dto.dict()) for dto in answer.result]
-    return ManyResponse[ExtendedPerson](total=answer.total, result=lst_person)
