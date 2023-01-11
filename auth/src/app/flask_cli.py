5d31489164fe7e0582dc1c7132a13c5de4c0d689
Sergey Morichev <s.morichev@yandex.ru>
2023-01-11 14:02:36 +0500

    refactor cli commands

--- auth/src/app/flask_cli.py
+++ auth/src/app/flask_cli.py
@@ -1,36 +1,59 @@
+import getpass
+
+import click
 from flask import Flask
-from app.models.db_models import db, Role, User
+
+from app.db import database
+from app.services import auth_service, role_service
+
+STANDARD_ROLES = "admin", "subscriber", "user"
 
 
 def init_cli(app: Flask):
     @app.cli.command("createsuperuser")
+    @click.option("--email", help="Superuser email")
+    @click.option("--password", help="Superuser password")
     def create_super_user(email, password):
-        pass
+        """Create superuser.
+
+        Email and password can be provided via --email and --password options.
+        Example: flask createsuperuser --email some_email --password some_password
+        """
+        if not email:
+            email = input("Enter email:")
+        if not password:
+            password = getpass.getpass("Enter password:")
+            while getpass.getpass("Confirm password:") != password:
+                continue
+
+        try:
+            # TODO mark as superuser
+            auth_service.register_user(email, password, email)
+        except auth_service.RegisterError:
+            click.echo("User with this email exists.")
+            return
+        click.echo("Superuser created")
 
     @app.cli.command("insert-roles")
     def insert_roles():
-        for role_name in ("admin", "subscriber", "user"):
-            db.session.add(Role(name=role_name))
-        db.session.commit()
-        print("roles inserted")
+        """Add standard roles to database on application deploy."""
+        for role_name in STANDARD_ROLES:
+            try:
+                role_service.add_role(role_name)
+            except role_service.RoleError:
+                pass
+        click.echo("Roles inserted")
 
     @app.cli.command("insert-users")
     def insert_users():
-        from werkzeug.security import generate_password_hash
-
-        default_role = db.session.query(Role).filter(Role.name == "user").first()
-        for user_email in ("example1", "example2", "example3"):
-            password_hash = generate_password_hash("password")
-            user = User(email=user_email, password_hash=password_hash)
-            user.roles.append(default_role)
-            db.session.add(user)
-        db.session.commit()
-        print("users created")
-
-        admin_role = db.session.query(Role).filter(Role.name == "admin").first()
-        password_hash = generate_password_hash("password")
-        admin_user = User(email="admin", password_hash=password_hash)
-        admin_user.roles.append(admin_role)
-        db.session.add(admin_user)
-        db.session.commit()
-        print("admin created")
+        """Add users and admin for development."""
+        for user_email in ("example1", "example2", "example3", "admin"):
+            try:
+                auth_service.register_user(user_email, "password", user_email)
+            except auth_service.RegisterError:
+                pass
+        click.echo("Users added")
+        admin = database.data.User.find_by_email("admin")
+        admin_role = database.data.Role.find_by_name("admin")
+        role_service.add_user_role(admin.id, admin_role.id)
+        click.echo("Admin role added")
