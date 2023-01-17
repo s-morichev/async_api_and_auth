a146e49faaab8094e90f5148d3dcc536fb2e1020
RomanBorovskiy <59736627+RomanBorovskiy@users.noreply.github.com>
2023-01-17 21:20:57 +0300

    add decorator check user roles

--- auth/src/app/utils/utils.py
+++ auth/src/app/utils/utils.py
@@ -1,5 +1,44 @@
 import hashlib
+from functools import wraps
+from flask_jwt_extended import get_jwt, jwt_required
+from flask_jwt_extended.exceptions import NoAuthorizationError
+
+from app.utils import constants
 
 
 def device_id_from_name(device_name: str):
     return hashlib.sha256(device_name.encode("utf8")).hexdigest()
+
+
+def jwt_accept_roles(roles_list: str | list[str]):
+    """
+    decorator for routers with accepted roles
+
+    :param roles_list - list of accepted roles like ["user","admin"]
+                        or string like "user, admin"
+    Grant access if roles_list INTERSECT user_roles NOT NULL
+    """
+
+    def decorator(f):
+        @jwt_required()
+        @wraps(f)
+        def decorated_function(*args, **kwargs):
+            if isinstance(roles_list, str):
+                accepted_roles = list(map(str.strip, roles_list.split(',')))
+            else:
+                accepted_roles = roles_list
+
+            token = get_jwt()
+            roles = token.get('roles', [])
+            # if superuser - dont check roles
+            if constants.ROOT_ROLE not in roles:
+                roles_intersect = set(accepted_roles) & set(roles)
+                if not roles_intersect:
+                    raise NoAuthorizationError(f'Only roles {accepted_roles} accepted')
+
+            rv = f(*args, **kwargs)
+            return rv
+
+        return decorated_function
+
+    return decorator
